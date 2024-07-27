987e30613a6ca8c566d18c372fc68f8d1ae799dd
Omidrng <108338782+Omidrng@users.noreply.github.com>
2024-07-27 15:53:49 +0330

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,6 +1,13 @@
-apt update
-apt upgrade
-apt install ufw -y
+#!/bin/bash
+
+# Update and upgrade system
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install UFW
+sudo apt install ufw -y
+
+# Add iptables rules
 sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
 sudo iptables -A FORWARD -s 102.0.0.0/8 -j DROP
 sudo iptables -A FORWARD -s 10.0.0.0/8 -j DROP
@@ -25,8 +32,16 @@ sudo iptables -A FORWARD -s 169.254.0.0/16 -j DROP
 sudo iptables -A FORWARD -s 198.18.140.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.230.9.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.233.71.0/24 -j DROP
-iptables-save
+
+# Save iptables rules
+sudo iptables-save
+
+# Set timezone to UTC
 sudo timedatectl set-timezone UTC
+
+# Run external scripts
 bash <(curl -s https://raw.githubusercontent.com/Kiya6955/IR-ISP-Blocker/main/ir-isp-blocker.sh)
 bash <(curl -Ls https://github.com/mikeesierrah/Marzban-scripts/raw/master/marzban-node.sh)
-iptables -L -v -n
+
+# List iptables rules
+sudo iptables -L -v -n
