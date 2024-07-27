bfc9c329130e01490f84739275b437c078ec9b52
Omidrng <108338782+Omidrng@users.noreply.github.com>
2024-07-27 16:13:56 +0330

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,5 +1,17 @@
-#!/bin/bash
+ufw enable
+ufw allow 22/tcp
+ufw allow 443/tcp
+ufw allow 80/tcp
 
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 172.16.0.0/12
+ufw deny out from any to 192.168.0.0/16
+ufw deny out from any to 100.64.0.0/10
+ufw deny out from any to 198.18.0.0/15
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 102.236.0.0/16
+ufw deny out from any to 2.60.0.0/16
+ufw deny out from any to 5.1.41.0/12
 
 sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
 sudo iptables -A FORWARD -s 102.0.0.0/8 -j DROP
@@ -26,10 +38,4 @@ sudo iptables -A FORWARD -s 198.18.140.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.230.9.0/24 -j DROP
 sudo iptables -A FORWARD -s 102.233.71.0/24 -j DROP
 iptables-save
-
-sudo timedatectl set-timezone UTC
-
-bash <(curl -s https://raw.githubusercontent.com/Kiya6955/IR-ISP-Blocker/main/ir-isp-blocker.sh)
-bash <(curl -Ls https://github.com/mikeesierrah/Marzban-scripts/raw/master/marzban-node.sh)
-
-sudo iptables -L -v -n
+sudo apt install iptables-persistent -y
