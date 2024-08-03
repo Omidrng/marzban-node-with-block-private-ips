48ea704d833f0b55d324202b16c4b94a5587645d
Omidrng <108338782+Omidrng@users.noreply.github.com>
2024-08-03 14:48:53 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -35,4 +35,35 @@ sudo timedatectl set-timezone UTC
 bash <(curl -s https://raw.githubusercontent.com/Kiya6955/IR-ISP-Blocker/main/ir-isp-blocker.sh)
 bash <(curl -Ls https://github.com/mikeesierrah/Marzban-scripts/raw/master/marzban-node.sh)
 
+
+
+apt install ufw
+ufw enable
+ufw allow 22/tcp
+ufw allow 80
+ufw allow 8080
+ufw allow 2096
+ufw allow 2082
+ufw allow 8443
+ufw allow 9528
+ufw allow 15932
+ufw allow 62050
+ufw allow 62051
+
+
+
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 172.16.0.0/12
+ufw deny out from any to 192.168.0.0/16
+ufw deny out from any to 100.64.0.0/10
+ufw deny out from any to 198.18.0.0/15
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 102.236.0.0/16
+ufw deny out from any to 2.60.0.0/16
+ufw deny out from any to 5.1.41.0/12
+
+
+
 iptables -L -v -n
+
+ufw status
