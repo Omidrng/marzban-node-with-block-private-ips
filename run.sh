e1e9b9fd4f2ca8e0a3a35d289e78d496d09f589a
Omidrng <108338782+Omidrng@users.noreply.github.com>
2024-08-05 14:48:16 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,15 +6,8 @@ apt install ufw -y
 
 sudo timedatectl set-timezone UTC
 
-bash <(curl -s https://raw.githubusercontent.com/Kiya6955/Abuse-Defender/main/abuse-defender.sh)
-
-wget https://github.com/Heclalava/blockpublictorrent-iptables/raw/main/bt.sh && chmod +x bt.sh && bash bt.sh
 
 bash <(curl -s https://raw.githubusercontent.com/Kiya6955/IR-ISP-Blocker/main/ir-isp-blocker.sh)
-
-apt update -y; apt install wireguard-dkms wireguard-tools resolvconf -y; wget https://github.com/ViRb3/wgcf/releases/download/v2.2.21/wgcf_2.2.21_linux_amd64; chmod +x wgcf_2.2.21_linux_amd64; mv wgcf_2.2.21_linux_amd64 /usr/bin/wgcf; wgcf register --accept-tos; wgcf generate; sed -i '2i\Table = off' wgcf-profile.conf; mkdir /etc/wireguard; cp wgcf-profile.conf /etc/wireguard/warp.conf; systemctl enable --now wg-quick@warp
-
-bash <(curl -Ls https://github.com/mikeesierrah/Marzban-scripts/raw/master/marzban-node.sh)
 
 
 apt install ufw
@@ -169,6 +162,49 @@ Subsystem	sftp	/usr/lib/openssh/sftp-server
 " > sshd_config
 
 sudo service ssh restart
+
+
+sudo iptables -A FORWARD -s 200.0.0.0/8 -j DROP
+sudo iptables -A FORWARD -s 102.0.0.0/8 -j DROP
+sudo iptables -A FORWARD -s 10.0.0.0/8 -j DROP
+sudo iptables -A FORWARD -s 100.64.0.0/10 -j DROP
+sudo iptables -A FORWARD -s 169.254.0.0/16 -j DROP
+sudo iptables -A FORWARD -s 198.18.0.0/15 -j DROP
+sudo iptables -A FORWARD -s 198.51.100.0/24 -j DROP
+sudo iptables -A FORWARD -s 203.0.113.0/24 -j DROP
+sudo iptables -A FORWARD -s 224.0.0.0/4 -j DROP
+sudo iptables -A FORWARD -s 240.0.0.0/4 -j DROP
+sudo iptables -A FORWARD -s 255.255.255.255/32 -j DROP
+sudo iptables -A FORWARD -s 192.0.0.0/24 -j DROP
+sudo iptables -A FORWARD -s 192.0.2.0/24 -j DROP
+sudo iptables -A FORWARD -s 127.0.0.0/8 -j DROP
+sudo iptables -A FORWARD -s 127.0.53.53 -j DROP
+sudo iptables -A FORWARD -s 192.168.0.0/16 -j DROP
+sudo iptables -A FORWARD -s 0.0.0.0/8 -j DROP
+sudo iptables -A FORWARD -s 172.16.0.0/12 -j DROP
+sudo iptables -A FORWARD -s 224.0.0.0/3 -j DROP
+sudo iptables -A FORWARD -s 192.88.99.0/24 -j DROP
+sudo iptables -A FORWARD -s 169.254.0.0/16 -j DROP
+sudo iptables -A FORWARD -s 198.18.140.0/24 -j DROP
+sudo iptables -A FORWARD -s 102.230.9.0/24 -j DROP
+sudo iptables -A FORWARD -s 102.233.71.0/24 -j DROP
+iptables-save
+ufw status
+
+
+ufw deny out from any to 10.0.0.0/8
+ufw deny out from any to 172.16.0.0/12
+ufw deny out from any to 192.168.0.0/16
+ufw deny out from any to 100.64.0.0/10
+ufw deny out from any to 198.18.0.0/15
+ufw deny out from any to 169.254.0.0/16
+ufw deny out from any to 102.236.0.0/16
+ufw deny out from any to 2.60.0.0/16
+ufw deny out from any to 5.1.41.0/12
+
+bash <(curl -Ls https://github.com/mikeesierrah/Marzban-scripts/raw/master/marzban-node.sh)
+
+
 
 iptables -L -v -n
 
